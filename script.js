616a2a5e04b4823d8f9b5d02cb3c93f030ac8485
robogf <111620315+robogf@users.noreply.github.com>
2022-09-29 20:10:44 -0700

    commited the first function for password generator

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword(){
+  passLength = prompt ('Choose the amount of characters youd like your password to have (8-128)')
+  if (passLength<8||passLength>128){
+    alert ('Please select a password length between 8 and 128 characters ')
+  } else if (isNaN(passLength)){
+  alert('Please enter a valid number')
+  } // This prompt allows the user to pick the amount of characters for the password and has them enter valid answers
+
+  
+
+}
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
